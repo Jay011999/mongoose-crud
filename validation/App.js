48165762ebebeee52cf40233f182105d420b6bdd
Jitendra Yadav <95463307+Jay011999@users.noreply.github.com>
2023-01-25 22:26:43 +0530

    Update App.js

--- validation/App.js
+++ validation/App.js
@@ -1,1 +1,21 @@
 
+const mongoose=require("mongoose");
+mongoose.connect("mongodb://localhost:27017/data");
+
+const fruitSchema  =new mongoose.Schema({
+  name:{ type:String,
+required:{true:'enter name of fruit'}},
+  rating:{
+    type:Number,
+    min:1,
+    max:10
+  }
+});
+
+const item= mongoose.model("First",fruitSchema);
+
+const element= new item({
+    rating:10
+});
+
+element.save();
