1cc6f0166789e23e3d98129998c7d2e0b3205436
Jitendra Yadav <95463307+Jay011999@users.noreply.github.com>
2023-01-25 22:35:01 +0530

    Update app.js

--- update-delete/app.js
+++ update-delete/app.js
@@ -1,1 +1,31 @@
 
+const mongoose=require("mongoose");
+mongoose.connect("mongodb://localhost:27017/data");
+
+const fruitSchema  =new mongoose.Schema({
+  name:{ type:String,
+required:{true:'enter name of fruit'}},
+  rating:{
+    type:Number,
+    min:1,
+    max:10
+  }
+});
+
+const item= mongoose.model("First",fruitSchema);
+
+item.deleteOne({name:"Apple"},function(err){
+    if(err){
+        console.log(err);
+    }else{
+        console.log("Deleted")
+    }
+})
+
+item.updateOne({_id:"63d15a406ca0b19952d13380"},{rating:6},function(err){
+    if(err){
+        console.log(err);
+    }else{
+        console.log("Success!!")
+    }
+})
